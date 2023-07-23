34a6feeb3f8a0f9284c237fadd100c2d934a3bff
YagoMatiasDev <yagomatiasdev@gmail.com>
2023-07-23 13:37:56 -0300

    slide banner com temporizador

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 import bannerImage from '../images/sportzoneBanner.png';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 const Banner = () => {
   return (
-    <div className="bannerConteiner">
-      <img src={bannerImage} alt="" />
-      );
-    </div>
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        // loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide className="swiper-slide">
+          <div>
+            <img src={bannerImage} alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <img src={bannerImage} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <img src={bannerImage} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <img src={bannerImage} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className="swiper-slide">
+          <img src={bannerImage} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
+
 export default Banner;
